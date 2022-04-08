6ac0ca71c1e8ab3a702a1acae39e05d2a863641a
Lucre <josielindamaldonado@gmail.com>
2022-04-07 23:36:44 -0400

    added click event on start button to start timer.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+var wordsArray = [
+  "JavaScript",
+  "variable",
+  "function",
+  "loop",
+  "conditional",
+  "object",
+  "array",
+];
+
+var startBtn = document.getElementById("start-button");
+
+var timeLeft = 10;
+
+startBtn.addEventListener("click", function () {
+  console.log("clicked");
+  var timeInterval = setInterval(function () {
+    if (timeLeft === 0) {
+      clearInterval(timeInterval);
+      console.log("Times up!");
+    } else {
+      console.log(timeLeft);
+      timeLeft--;
+    }
+  }, 1000);
+});
