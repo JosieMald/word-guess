9c164055b8b241f500853ac03ad954bcadb11bf6
Lucre <josielindamaldonado@gmail.com>
2022-04-08 00:37:32 -0400

    added toggle user cannot guess while game is not in play.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -12,32 +12,39 @@ var startBtn = document.getElementById("start-button");
 var timerEL = document.getElementById("timer");
 var currentWord = document.getElementById("hidden-word");
 
-
-var timeLeft = 10;
-var hiddenWord = [];
+var hiddenWord = "";
 var lettersGuessed = [];
+var toggle = "off";
 
 startBtn.addEventListener("click", function () {
-    displayWord();
+    var timeLeft = 10;
+    lettersGuessed = [];
+    toggle = "on";
+  displayWord();
   var timeInterval = setInterval(function () {
     if (timeLeft === 0) {
       clearInterval(timeInterval);
       console.log("Times up!");
+      toggle = "off";
     } else {
-        timeLeft--;
+      timeLeft--;
       timerEL.innerHTML = timeLeft + " seconds remaining";
     }
   }, 1000);
 });
 
 function displayWord() {
-    hiddenWord.push(wordsArray[0]);
-currentWord.innerHTML = wordsArray[0];
-console.log(hiddenWord);
+  var index = Math.floor(Math.random() * wordsArray.length);
+  hiddenWord = wordsArray[index];
+  currentWord.innerHTML = wordsArray[index];
 }
 
-document.addEventListener("keydown", function(event){
-    var letter = event.key;
-    lettersGuessed.push(letter);
-    console.log(lettersGuessed);
-})
+document.addEventListener("keydown", function (event) {
+    if(toggle == "on"){
+        var letter = event.key;
+        lettersGuessed.push(letter);
+        console.log(lettersGuessed);
+    } else {
+        return;
+    }
+});
