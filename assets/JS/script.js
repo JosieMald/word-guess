740da2526457bbd4c5b6b130f918321d01d6750f
Lucre <josielindamaldonado@gmail.com>
2022-04-08 23:13:06 -0400

    letters guessed by user that are correct now display on the screen. If user wins or losses a message will apear and wins or loses are tracked.'

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -8,35 +8,59 @@ var wordsArray = [
   "array",
 ];
 
-var startBtn = document.getElementById("start-button");
+var startBtnEl = document.getElementById("start-button");
 var timerEL = document.getElementById("timer");
-var currentWord = document.getElementById("hidden-word");
+var currentWordEl = document.getElementById("hidden-word");
 
-var hiddenWord = "";
 var lettersGuessed = [];
 var toggle = "off";
+var wins = 0;
+var losses = 0;
 
-startBtn.addEventListener("click", function () {
+startBtnEl.addEventListener("click", function () {
     var timeLeft = 10;
     lettersGuessed = [];
     toggle = "on";
-  displayWord();
-  var timeInterval = setInterval(function () {
-    if (timeLeft === 0) {
-      clearInterval(timeInterval);
-      console.log("Times up!");
-      toggle = "off";
-    } else {
-      timeLeft--;
-      timerEL.innerHTML = timeLeft + " seconds remaining";
-    }
-  }, 1000);
+    displayWord();
+    var timeInterval = setInterval(function () {
+        if (timeLeft === 0) {
+            clearInterval(timeInterval);
+            currentWordEl.innerHTML = "YOU LOST!!!"
+            toggle = "off";
+                losses++;
+            console.log("This is loses: " + losses);
+        } else if (stopTimer == true) {
+            clearInterval(timeInterval);
+            stopTimer = false;
+            wins++;
+            console.log("This is wins: " + wins);
+        }
+        else {
+            timeLeft--;
+            timerEL.innerHTML = timeLeft + " seconds remaining";
+        }
+    }, 1000);
 });
 
+var hiddenWord = "";
+var stopTimer = false;
+var index = Math.floor(Math.random() * wordsArray.length);
+hiddenWord = wordsArray[index];
 function displayWord() {
-  var index = Math.floor(Math.random() * wordsArray.length);
-  hiddenWord = wordsArray[index];
-  currentWord.innerHTML = wordsArray[index];
+    var hiddenWordBlank = "";
+  for (var i = 0; i < hiddenWord.length; i++){
+      if(lettersGuessed.includes(hiddenWord[i])){
+          hiddenWordBlank = hiddenWordBlank + " " + hiddenWord[i] + " "
+        }  else {
+            hiddenWordBlank = hiddenWordBlank + " _ "
+        }
+    }
+    currentWordEl.innerHTML = hiddenWordBlank;
+    var removedSpaces = hiddenWordBlank.split(" ").join("");
+    if(hiddenWord === removedSpaces) {
+        currentWordEl.innerHTML = "YOU WON!!!"
+        stopTimer = true;
+    }
 }
 
 document.addEventListener("keydown", function (event) {
@@ -44,6 +68,7 @@ document.addEventListener("keydown", function (event) {
         var letter = event.key;
         lettersGuessed.push(letter);
         console.log(lettersGuessed);
+        displayWord();
     } else {
         return;
     }
